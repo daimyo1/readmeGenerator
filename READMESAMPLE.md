8d959cadee3b97a1536119322f02afd5f650b91b
Jose Arambula <163930521+daimyo1@users.noreply.github.com>
2024-07-08 18:05:43 -0700

    Add files via upload

---
+++ READMESAMPLE.md
@@ -0,0 +1,33 @@
+# Project Read ME
+
+![License](https://img.shields.io/badge/License-Apache 2.0-blue.svg)
+
+## Description
+This is a tutorial on generating a readme.me
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Follow the prompts from your command promt!
+
+## Usage
+You run the commands through your command promt!
+
+## License
+This project is licensed under the Apache 2.0 license. For more information, please visit [Apache 2.0 License](https://opensource.org/licenses/Apache-2.0).
+
+## Contributing
+Just dont fork it!
+
+## Tests
+Run node index.js
+
+## Questions
+If you have any questions, you can find me on GitHub: [Daimyo1](https://github.com/Daimyo1).
+You can also reach me at my email: Jgrambula@gmail.com.
---
+++ index.js
@@ -0,0 +1,76 @@
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this application?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide guidelines for contributing:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing README file:', err);
+    } else {
+      console.log('README file generated successfully!');
+    }
+  });
+}
+
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdownContent = generateMarkdown(answers);
+    writeToFile('README.md', markdownContent);
+  });
+}
+
+
+init();
